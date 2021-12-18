3c41fae45965d39507235b25b235cf39ce90259a
Mohammad-Fawaz/Mohammad-Fawaz <fawaz_farhat@hotmail.com>
2021-12-18 13:51:30 +0500

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export class Friend {
+  constructor(
+    public breed: string,
+    public dateOfBirth: Date,
+    public id: string,
+    public name: string
+  ) {
+  }
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  friends: Friend[];
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.getFriends();
+  }
+
+  getFriends(){
+    this.httpClient.get<any>('http://demo.kioskgrp.com:8080/pets').subscribe(
+      response => {
+        console.log(response);
+        this.friends = response;
+      }
+    );
+  }
+}
